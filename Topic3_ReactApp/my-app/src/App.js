5b2ba342b0c1e60d3cc15c7d6a7356bcd24c80ce
ThoPN <thopn3@fpt.edu.vn>
2023-10-04 11:59:15 +0700

    Component in React

--- Topic3_ReactApp/my-app/src/App.js
+++ Topic3_ReactApp/my-app/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// Nhập khẩu component 'Product' để sử dụng
+import Product from "./components/Product";
 
+const products = [
+  {'id': 1, 'name': 'Macbook Pro M1'},
+  {'id': 2, 'name': 'Samsung Note 9'},
+  {'id': 3, 'name': 'IPhone 15 ProMax'}
+]
+
+// Component
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to React application</h1>
+      {/* Gọi component */}
+      {/* <Product id='P001' name='Iphone 15 ProMax Titan'/> */}
+      <Product data={products}/>
     </div>
   );
 }
---
+++ Topic3_ReactApp/my-app/src/components/Product.js
@@ -0,0 +1,12 @@
+// Sử dụng function để định nghĩa 1 component
+function Product(p){
+    return (
+        <div>
+            <h2>List of products</h2>
+            
+        </div>
+    )
+}
+
+// Xuất bản component này (export module)
+export default Product
